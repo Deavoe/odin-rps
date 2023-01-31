977b42735e3c2c39c2d936832f39f5e25c6d973a
Deavoe <123446640+Deavoe@users.noreply.github.com>
2023-01-31 13:14:04 -0500

    Create game playthrough logic and function

--- rps.js
+++ rps.js
@@ -5,8 +5,8 @@ return moves[~~(Math.random() * moves.length)]
 
 function rps(player, cpu) {
  if (typeof player === "string" && typeof cpu === "string") {
-let plr = player.toLowerCase()
-let cp = cpu.toLowerCase()
+ let plr = player.toLowerCase()
+ let cp = cpu.toLowerCase()
   if (cp === "scissors" && plr === "rock" || cp === "paper" && plr === "scissors" || cp === "rock" && plr === "paper" ) {
     return "You win. " + plr.replace(plr[0],plr[0].toUpperCase())  + " beats " + cp.replace(cp[0],cp[0].toUpperCase()) + "."
   } else if (plr === "scissors" && cp === "rock" || plr === "paper" && cp === "scissors" || plr === "rock" && cp === "paper" )  {  
@@ -19,6 +19,20 @@ let cp = cpu.toLowerCase()
  }
 }
 
-const playerSelection = "rock"; // testing
-const computerSelection = getComputerChoice();
-console.log(rps(playerSelection, computerSelection));
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Input rock, paper, or scissors - spell it exact.") // "rock"; // testing
+        while (typeof playerSelection !== "string" || (playerSelection.toLowerCase() !== "rock" && playerSelection.toLowerCase() !== "paper" && playerSelection.toLowerCase() !== "scissors")) {
+            playerSelection = prompt("Input rock, paper, or scissors - spell it exact.")
+            if (typeof playerSelection === "string") {
+                if (playerSelection.toLowerCase() === "rock" || playerSelection.toLowerCase() === "scissors" || playerSelection.toLowerCase() === "paper") {
+                break;
+                }
+            }
+        }
+        const computerSelection = getComputerChoice();
+        console.log(rps(playerSelection, computerSelection));
+    }
+    alert("Game over. Refresh the page.")
+}
+game()
