a647cd1f650ffcf5ef390b3f0b6833661a93c072
Deavoe <123446640+Deavoe@users.noreply.github.com>
2023-01-31 12:49:54 -0500

    Create function for a round of RPS and an AI's move

--- rps.js
+++ rps.js
@@ -1,1 +1,24 @@
-console.log("Hello world!")
+function getComputerChoice(cpu) {
+let moves = ['rock','paper','scissors']
+return moves[~~(Math.random() * moves.length)]
+}
+
+function rps(player, cpu) {
+ if (typeof player === "string" && typeof cpu === "string") {
+let plr = player.toLowerCase()
+let cp = cpu.toLowerCase()
+  if (cp === "scissors" && plr === "rock" || cp === "paper" && plr === "scissors" || cp === "rock" && plr === "paper" ) {
+    return "You win. " + plr.replace(plr[0],plr[0].toUpperCase())  + " beats " + cp.replace(cp[0],cp[0].toUpperCase()) + "."
+  } else if (plr === "scissors" && cp === "rock" || plr === "paper" && cp === "scissors" || plr === "rock" && cp === "paper" )  {  
+    return "You lose. " + cp.replace(cp[0],cp[0].toUpperCase())  + " beats " + plr.replace(plr[0],plr[0].toUpperCase()) + "."
+  } else if (plr === cp) {  
+    return "Tie."
+  } else {
+    return "Invalid inputs.";
+  }
+ }
+}
+
+const playerSelection = "rock"; // testing
+const computerSelection = getComputerChoice();
+console.log(rps(playerSelection, computerSelection));
